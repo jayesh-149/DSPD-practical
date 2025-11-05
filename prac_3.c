6faeb24c5c578d15d4f1b85eeed6da6252008aac
jayesh-149 <matejayesh149@gmail.com>
2025-11-05 20:58:04 +0530

    Add files via upload

---
+++ prac_3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+struct Student {
+    int rollNo;
+    char name[50];
+    int age;
+    float avgMarks;
+};
+int main() {
+    struct Student s[100], temp;
+    int n;
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("\nEnter details of student %d:\n", i + 1);
+        printf("Roll No: ");
+        scanf("%d", &s[i].rollNo);
+        printf("Name: ");
+        scanf("%s", s[i].name);
+        printf("Age: ");
+        scanf("%d", &s[i].age);
+        printf("Average Marks: ");
+        scanf("%f", &s[i].avgMarks);
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (s[j].avgMarks < s[j + 1].avgMarks) {
+                temp = s[j];
+                s[j] = s[j + 1];
+                s[j + 1] = temp;
+            }
+        }
+    }
+    printf("\nStudents Details\n");
+    printf("RollNo   Name  Age   AvgMarks\n");
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d\t %s\t %d\t %.2f\n",
+               s[i].rollNo, s[i].name, s[i].age, s[i].avgMarks);
+    }
+    return 0;
+}
